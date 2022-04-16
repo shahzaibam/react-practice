9a0b23c2b6ca6c7deb6b656e8aa0a0e0ab974e07
Shah <shebiraaja@gmail.com>
2022-04-16 23:38:53 +0200

    modifing the actividades trying 2 do somthng new

--- src/Components/actividades.js
+++ src/Components/actividades.js
@@ -20,6 +20,17 @@ const actividades = [
         },
         comments: [{ from: "Julia", text: "Me too!" }, { from: "Julia", text: "Me too!" }, { from: "Julia", text: "Me too!" }]
     }
+
+    {
+        timestamp: new Date().getTime(),
+        text: "Played fifa",
+        user: {
+            id: 2,
+            name: "ElJorge",
+            avatar: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png",
+        },
+        comments: [{ from: "Julia", text: "Me too!" }, { from: "Julia", text: "Me too!" }, { from: "Julia", text: "Me too!" }]
+    }
 ];
 
 export default actividades;
