0e1e38ff876411f34d0041318b6d868793277950
Shah <shebiraaja@gmail.com>
2022-05-01 13:44:48 +0200

    made a static clock that doesn't updated automatically

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -11,7 +11,16 @@ const Clock = () => {
 
 
   return (
-    <div>Clock</div>
+    <div className='clock'>
+
+    {hours == 0 ? 12 : (hours > 12) ? hours - 12 : hours}
+    :
+    {minutes > 9 ? minutes : `0${minutes}`}
+    :
+    {seconds > 9 ? seconds : `0${seconds}`}
+
+    
+    </div>
   )
 }
 
