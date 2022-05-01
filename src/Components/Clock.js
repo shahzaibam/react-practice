11b32fa15d8b40cadf8df7377987b5b897f59c78
Shah <shebiraaja@gmail.com>
2022-05-01 14:06:15 +0200

    practicing to make it with usestate

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,27 +1,41 @@
 import React from 'react'
 
-const Clock = () => {
+class Clock extends React.Component {
 
-    const currentTime = new Date()
-    const hours = currentTime.getHours()
-    const minutes = currentTime.getMinutes()
-    const seconds = currentTime.getSeconds()
-    const ampm = hours >= 12 ? "pm" : "am"
+    constructor(props) {
+        super(props);
+        this.state = this.getTime();
+    }
+
+    getTime() {
+        const currentTime = new Date();
+        return {
+            hours: currentTime.getHours(),
+            minutes: currentTime.getMinutes(),
+            seconds: currentTime.getSeconds(),
+            ampm: currentTime.hours >= 12 ? "pm" : "am"
+
+        }
+    }
 
 
+    render() {
+        const {hours, minutes, seconds, ampm} = this.state
+        return (
+            <div className='clock'>
 
-  return (
-    <div className='clock'>
+                {hours == 24 ? 0 : (hours > 12) ? hours : hours - 12}
+                :
+                {minutes > 9 ? minutes : `0${minutes}`}
+                :
+                {seconds > 9 ? seconds : `0${seconds}`}
 
-    {hours == 24 ? 0 : (hours > 12) ? hours : hours - 12}
-    :
-    {minutes > 9 ? minutes : `0${minutes}`}
-    :   
-    {seconds > 9 ? seconds : `0${seconds}`}
 
-    
-    </div>
-  )
+            </div>
+        )
+    }
+
 }
+
 
 export default Clock
